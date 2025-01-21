1e417f48e86bf360696a65886b007011ee6ba4ad
Col-R <2colewestin@gmail.com>
2025-01-21 13:02:59 -0600

    satisfied typescript with import Promise and types folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { getProjects } from "@/sanity/sanity-utils";
 
 export default async function Home() {
-  const projects = await getProjects();
+  const projects = await getProjects(); // projects fetched as Promise, type checked in sanity-utils.ts
   console.log("Fetched projects:", projects); // Debugging statement
 
   return (
@@ -9,7 +9,11 @@ export default async function Home() {
       {projects.length === 0 ? (
         <div>No projects found</div>
       ) : (
-        projects.map((project) => <div key={project._id}>{project.name}</div>)
+        projects.map((project) => (
+          <div key={project._id}>
+            <h1>{project.name}</h1>
+          </div>
+        ))
       )}
     </div>
   );
