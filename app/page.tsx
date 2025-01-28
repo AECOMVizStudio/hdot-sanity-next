3d5e9c5689aaba3cc42f7f2ae759e548b937ef22
Col-R <2colewestin@gmail.com>
2025-01-28 14:42:12 -0600

    got home page data from sanity to render on frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // n https://www.youtube.com/watch?v=OcTPaUfay5I
-import { getProjects } from "@/sanity/sanity-utils";
+import { getHomePage } from "@/sanity/sanity-utils";
 import { PortableText } from "next-sanity";
 
 import PhotoGallery from "@/components/PhotoGallery/PhotoGallery";
@@ -7,21 +7,22 @@ import Section from "@/components/Section/Section";
 import Button from "@/components/Button/Button";
 import Link from "next/link";
 
+import { HomePage } from "@/types/HomePage";
+
 export default async function Home() {
-  const projects = await getProjects(); // projects fetched as Promise, type checked in sanity-utils.ts
-  // console.log("Fetched projects:", projects);  Debugging statement
+  const homePage = await getHomePage();
+  console.log("Fetched home page data:", homePage); // Debugging statement
 
   return (
     <>
-      <PhotoGallery />
       <Section title="Project Overview">
-        <p>Project overview text goes here...</p>
+        <PortableText value={homePage[0].projectOverview} />
       </Section>
       <Section title="Project Updates & Events">
-        <p>Project updates and events text goes here...</p>
+        <PortableText value={homePage[0].projectUpdates} />
       </Section>
       <Section title="Leave a Comment">
-        <p>If you have any comments, please leave them below:</p>
+        <PortableText value={homePage[0].leaveComment} />
         <Link href="/submit-comments">
           <Button>Leave a Comment</Button>
         </Link>
