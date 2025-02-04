cf1f79b466624f6276f118112ca902fd36e8ff43
Col-R <2colewestin@gmail.com>
2025-02-04 09:59:12 -0600

    updated gitignore and sanity CDN config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["cdn.sanity.io"], // Add Sanity CDN domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/**`,
+      },
+    ],
   },
 };
 
