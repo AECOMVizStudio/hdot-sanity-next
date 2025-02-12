37d0484dde6131e85805f0cc90d4c3f12f12ea7c
Col-R <2colewestin@gmail.com>
2025-02-12 09:17:48 -0600

    added alt text and resized logo

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div className="text-xl font-bold flex flex-row items-center">
-      <Image src="/hdot.svg" alt="Logo" width={100} height={100} />
+      <Image src="/hdot.svg" alt="HDOT Logo" width={75} height={75} />
       <div className="pl-4">Hawaii Department of Transportation</div>
     </div>
   );
