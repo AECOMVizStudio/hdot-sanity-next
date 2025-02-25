c2331268065ad25e7c4cab0fcd28e143a0c2d8bd
Col-R <2colewestin@gmail.com>
2025-02-25 12:43:45 -0600

    image imports done

--- components/ProjectInfoCard/ProjectInfoCard.tsx
+++ components/ProjectInfoCard/ProjectInfoCard.tsx
@@ -13,11 +13,23 @@ function ProjectInfoCard(props: Card) {
       <h2 className="text-2xl font-bold mb-4 ml-2 text-hdotTeal text-left">
         {props.title}
       </h2>
-      <div className="w-full mb-4"></div>
+      <div className="w-full h-[250px] mb-4 relative">
+        <Image
+          src={props.image.asset.url}
+          alt={props.alt}
+          layout="fill"
+          objectFit="cover"
+          className="w-full"
+        />
+      </div>
       <ul className="list-disc list-inside mb-4">
-        <li>Bullet point 1</li>
-        <li>Bullet point 2</li>
-        <li>Bullet point 3</li>
+        {props.bulletPoints.map((bulletPoint, index) => {
+          return (
+            <li key={index} className="text-hdotTeal text-lg">
+              {bulletPoint}
+            </li>
+          );
+        })}
       </ul>
       <div className="text-center">
         <button className="text-hdotTeal font-bold">Expand</button>
