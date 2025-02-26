16d30e3c93c3694d4d79c3e304d9e6915730464f
Col-R <2colewestin@gmail.com>
2025-02-26 11:01:33 -0600

    moved children text below image on isTimeline === true

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -24,15 +24,15 @@ const Section = ({
         <div className="w-full md:w-1/2 text-center md:text-left p-4">
           <h2 className="text-3xl font-semibold text-hdotTeal mb-4">{title}</h2>
           <hr className="border-t-2 border-hdotTeal my-4 w-full md:w-1/2" />
-          <div className="text-hdotHoverTeal text-lg">{children}</div>
         </div>
         <Image
           src={imageSrc}
           alt={altText}
           width={1000} // Adjust width as needed
-          height={300} // Adjust height as needed
+          height={400} // Adjust height as needed
           className=""
         />
+        <div className="text-hdotHoverTeal text-lg">{children}</div>
       </div>
     );
   } else {
